c75419d5122d96c02c35c4fbf7cd578fdcf4986e
Waseem Ahmed <alphawaseem@gmail.com>
2017-03-27 16:17:59 +0530

    use jinja2

--- main.py
+++ main.py
@@ -13,13 +13,26 @@
 # limitations under the License.
 
 import webapp2
+import os 
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape = True)
 
 
-class MainPage(webapp2.RequestHandler):
+class Handler(webapp2.RequestHandler):
+    def write(self,*a,**kw):
+        self.response.out.write(*a,**kw)
+    def render_str(self,template,**params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    def render(self,template,**kw):
+        self.write(self.render_str(template,**kw))
+
+class MainPage(Handler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
-
+        self.write('Hello World')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
