eef9f8c76ac16a61901c2e4bd8ac58f42a0e8207
Waseem Ahmed <alphawaseem@gmail.com>
2017-04-02 19:58:14 +0530

    define all possible helper methods

--- main.py
+++ main.py
@@ -16,14 +16,81 @@ import webapp2
 import os
 import jinja2
 import hashlib
+import re
+import random
+import hmac
+from string import letters
 
 from google.appengine.ext import ndb
-# from google.cloud import datastore
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
+
+secret = '$he$lloworld$welcometo$myblog$'
+
+
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
+
+
+def make_salt(length=5):
+    return ''.join(random.choice(letters) for x in range(length))
+
+
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha512(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
+9
+
+def users_key(group='default'):
+    return ndb.Key.from_path('users', group)
+
+
+class User(ndb.Model):
+    firstname = ndb.StringProperty(required=True)
+    lastname = ndb.StringProperty(required=True)
+    joined = ndb.DateTimeProperty(auto_now_add=True)
+    email = ndb.StringProperty(required=True)
+    pass_hash = ndb.TextProperty(required=True)
+
+    @classmethod
+    def by_id(cls,uid):
+        return User.get_by_id(uid,parent=users_key())
+        
+    @classmethod
+    def by_email(cls,email):
+        u = User.all().filter('email =',email).get()
+        return u
+
+    @classmethod
+    def register(cls,firstname,lastname,pw,email):
+        pw_hash = make_pw_hash(email,pw)
+        return User(parent=users_key(),
+                    firstname = firstname,
+                    lastname = lastname,
+                    pass_hash=pw_hash,
+                    email = email)
+    
+    @classmethod
+    def login(cls,email,pw):
+        u = cls.by_email(email)
+        if u and valid_pw(email,pw,u.pass_hash):
+            return u
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -36,6 +103,27 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % (name, cookie_val))
+
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
+
+    def login(self, user):
+        self.set_secure_cookie('user_id', str(user.key().id()))
+
+    def logout(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
+
 
 class MainPage(Handler):
     def get(self):
@@ -47,7 +135,7 @@ class RegisterHandler(Handler):
         self.render("register.html")
 
     def post(self):
-        self.write('Register user to database handler')
+        firstname = self.request.get()
 
 
 class LoginHandler(Handler):
