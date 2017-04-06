502990fcf6f64bfe019a528de7a8e4cf4e8cb8ef
Waseem Ahmed <alphawaseem@gmail.com>
2017-04-06 19:40:48 +0530

    Document cookielib

--- cookielib.py
+++ cookielib.py
@@ -1,13 +1,27 @@
+"""
 
+This Module helps to encrypt and decrypt cookies using
+hmac library
+
+"""
 
 import hmac
 
+# My Secret Key
 secret = '$he$lloworld$welcometo$myblog$'
 
 def encrypt_cookie_value(val):
+    """
+    This Function encrypts the given value using hmac and 
+    a secret key
+    """
     return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
 
 def decrypt_cookie_value(secure_val):
+    """
+    This function takes the encrypted value and validates
+    it then returns the value
+    """
     val = secure_val.split('|')[0]
     if secure_val == encrypt_cookie_value(val):
         return val
