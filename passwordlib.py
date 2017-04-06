4ca6d88b56ed61122892fe06a4c0270fe6c63d58
Waseem Ahmed <alphawaseem@gmail.com>
2017-04-06 19:50:19 +0530

    document passwordlib

--- passwordlib.py
+++ passwordlib.py
@@ -1,11 +1,28 @@
+"""
+This module helps to hash passwords with random salts
+"""
+
+# Import neccessary modules
 import random
 from string import letters
 import hashlib
+
+
 def make_salt(length=5):
+    """
+    returns a random salt of default length of 5
+    """
     return ''.join(random.choice(letters) for x in range(length))
 
 
 def make_pw_hash(name, pw, salt=None):
+    """
+    returns a salt and hashed value  in the format %s,%salt
+    If salt in not given then it creates a new salt and returns
+    hashed value with salt. Else use that salt to generate hash this 
+    is useful when verifying the hash with raw values. 
+    It uses sha512 and mix of name password and salt.
+    """
     if not salt:
         salt = make_salt()
     h = hashlib.sha512(name + pw + salt).hexdigest()
@@ -13,5 +30,10 @@ def make_pw_hash(name, pw, salt=None):
 
 
 def verify_pw_hash(name, password, h):
+    """
+    returns True/False if name and password generates same given hash
+    which contains salt. We use this salt value to regenerate the hash
+    and match it with given hash.
+    """
     salt = h.split(',')[0]
     return h == make_pw_hash(name, password, salt)
